23396e0b02a4939ea2f6c720a79f2aa2796f35d6
KhangNguyennnn1 <Duy.K.Nguyen-2@student.uts.edu.au>
2025-05-20 13:42:04 +1000

    payment.java
    add in payment java

---
+++ src/main/java/uts/isd/model/Payment.java
@@ -0,0 +1,131 @@
+package uts.isd.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Payment implements Serializable {
+    private int id;
+    private int orderId;
+    private String paymentMethod;
+    private String cardNumber;
+    private String cardHolderName;
+    private String expiryDate;
+    private String cvv;
+    private double amount;
+    private Date paymentDate;
+    private String status;  // e.g., "pending", "completed", "cancelled"
+
+    public Payment() { }
+
+    public Payment(int id, int orderId, String paymentMethod, String cardNumber, 
+                  String cardHolderName, String expiryDate, String cvv, 
+                  double amount, Date paymentDate, String status) {
+        this.id = id;
+        this.orderId = orderId;
+        this.paymentMethod = paymentMethod;
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+        this.expiryDate = expiryDate;
+        this.cvv = cvv;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+        this.status = status;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "id=" + id +
+                ", orderId=" + orderId +
+                ", paymentMethod='" + paymentMethod + '\'' +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", cardHolderName='" + cardHolderName + '\'' +
+                ", expiryDate='" + expiryDate + '\'' +
+                ", cvv='" + cvv + '\'' +
+                ", amount=" + amount +
+                ", paymentDate=" + paymentDate +
+                ", status='" + status + '\'' +
+                '}';
+    }
+} 
